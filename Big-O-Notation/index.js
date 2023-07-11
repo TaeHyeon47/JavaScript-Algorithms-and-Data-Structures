35a3514840a88e98f05c96346ad39ed9009c51a8
Tae Hyeon Jo <hillskuti@naver.com>
2023-07-11 22:52:01 +0900

    코드 속도 비교 테스트

--- Big-O-Notation/index.js
+++ Big-O-Notation/index.js
@@ -1,10 +1,24 @@
+// Big-O-Notation
+
 // 코드 시간 재기
+// 1부터 n까지의 숫자의 합
+
+// 1.
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 1; i <= n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// 2.
+// 아래의 코드가 더 빠름
 function addUpTo(n) {
-  let total = 0;
-  for (let i = 1; i <= n; i++) {
-    total += i;
-  }
-  return total;
+  return (n * (n + 1)) / 2;
 }
 
-console.log(addUpTo(6));
+let t1 = performance.now();
+console.log(addUpTo(1000000000));
+let t2 = performance.now();
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
