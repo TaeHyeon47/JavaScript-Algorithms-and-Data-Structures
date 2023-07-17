876e169e64169df3a35c87d057cb9a978b2877fe
Tae Hyeon Jo <hillskuti@naver.com>
2023-07-17 21:48:42 +0900

    빅오 예시 추가(햇갈릴 수 있는 함수)

--- Big-O-Notation/index.js
+++ Big-O-Notation/index.js
@@ -1,9 +1,9 @@
 // Big-O-Notation
-
-// 코드 시간 재기
+///////////////////////////////////////////////////
+// 1. 코드 시간 재기
 // 1부터 n까지의 숫자의 합
 
-// 1.
+// 1)
 // function addUpTo(n) {
 //   let total = 0;
 //   for (let i = 1; i <= n; i++) {
@@ -12,13 +12,29 @@
 //   return total;
 // }
 
-// 2.
+// 2)
 // 아래의 코드가 더 빠름
-function addUpTo(n) {
-  return (n * (n + 1)) / 2;
+// function addUpTo(n) {
+//   return (n * (n + 1)) / 2;
+// }
+
+// let t1 = performance.now();
+// console.log(addUpTo(1000000000));
+// let t2 = performance.now();
+// console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+
+////////////////////////////////////////////////////
+// 2. 빅 O 예시
+// 1) O(n)
+function logAtLeast5(n) {
+  for (var i = 1; i <= Math.max(5, n); i++) {
+    console.log(i);
+  }
 }
 
-let t1 = performance.now();
-console.log(addUpTo(1000000000));
-let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds`);
+// 2) O(1)
+function logAtMost5(n) {
+  for (var i = 1; i <= Math.min(5, n); i++) {
+    console.log(i);
+  }
+}
