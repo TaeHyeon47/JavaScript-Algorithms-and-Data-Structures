8955a65ed8e032fdd5c6502e99a02a59412406e4
Tae Hyeon Jo <hillskuti@naver.com>
2023-07-18 21:18:53 +0900

    공간복잡도 샘플 코드

--- Big-O-Notation/index.js
+++ Big-O-Notation/index.js
@@ -38,3 +38,13 @@ function logAtMost5(n) {
     console.log(i);
   }
 }
+
+/////////////////////////////////////////////////////
+// 3. 공간 복잡도
+function double(arr) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(2 * arr[i]);
+  }
+  return newArr;
+}
