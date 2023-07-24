fe1a120246ebe97d3f79101db53deccf9c7ade1e
Tae Hyeon Jo <hillskuti@naver.com>
2023-07-24 21:30:00 +0900

    문제 세부 분석

--- Big-O-Notation/index2.js
+++ Big-O-Notation/index2.js
@@ -1,7 +1,12 @@
+// 문제 해결을 위한 전체 프로세스
+// 1. Understand the Problem
+// 2. Explore Concrete Examples
+// 3. Break It Down - Explicitly write out the steps you need to take.
+
+//? 첫번째 - 문제 이해(정의)
 // ===============================================================
 // Write a function which takes two numbers and returns their sum.
 // ===============================================================
-
 // 1. Can I restate the problem in my own words?
 'implement addition';
 // 2. What are the inputs that go into the problem?
@@ -13,6 +18,7 @@
 // 4. Can the outputs be determined from the inputs? In other words, do I have enough information to solve the problem?
 // 5. How should I label the important pieces of data that are a part of the problem?
 
+//? 두번째 - 구체적인 예시 탐색
 // Wrote a function which takes in a string and returns counts of each character in the string.
 // 1) 간단한 입력 값을 생각하며 문제를 정의할 수 있다.
 charCount('aaaaa'); // {a : 4}
@@ -24,3 +30,52 @@ charCount('hello'); // {h:1, e:1, l:2, 0:1}
 
 // 3) 빈 입력 값이 주어진 예시를 생각한다.
 charCount(''); // 빈 문자열, 객체, null, undefined를 입력했을 때의 예시를 생각해본다.
+
+//? 세번째 - 단계별로 문제 작성해 나가기
+// Write a function which takes in a string and returns counts of each character in the string.
+
+charCount('aaaa');
+/* { 
+   a: 4
+} */
+
+charCount('hello');
+/* {
+   h: 1,
+   e: 1,
+   l: 2,
+   o: 1
+} */
+
+charCount('Your PIN number is 1234!');
+/* {
+    1: 1,
+    2: 1,
+    3: 1,
+    4: 1,
+    b: 1,
+    e: 1,
+    i: 2,
+    m: 1,
+    n: 2,
+    o: 1,
+    p: 1,
+    r: 2,
+    s: 1,
+    u: 2,
+    y: 1
+} */
+
+function charCount(str) {
+  // do something
+  // return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts for those characters.
+}
+
+function charCount(str) {
+  // make object to return at end
+  // loop over string, for each character...
+  /// if the char is a number/letter AND a key in object, add one to count
+  /// if the char is a number/letter AND not in object, add it to object and set value to 1
+  /// if character is something else (space, period, etc.) don't do anyting
+  // return object at end
+}
