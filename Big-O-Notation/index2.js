572cbbfb0048ef9a9983dbc0801368f2d09518da
Tae Hyeon Jo <hillskuti@naver.com>
2023-08-03 23:07:18 +0900

    빈도 수 세기 패턴

--- Big-O-Notation/index2.js
+++ Big-O-Notation/index2.js
@@ -144,4 +144,18 @@ function isAlphaNumeric(char) {
   return true;
 }
 
-charCodeAt;
+// 빈도수 세기 패턴
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    arr2.splice(correctIndex, 1);
+  }
+  return;
+}
