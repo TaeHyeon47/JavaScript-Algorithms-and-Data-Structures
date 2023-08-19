9ce4580a7673d71981b8c09029337ebeec469381
Tae Hyeon Jo <hillskuti@naver.com>
2023-08-19 23:49:41 +0900

    다중 포인터 패턴을 위한 합계 확인 코드

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -72,3 +72,11 @@ function validAnagram(first, second) {
 // array of integers. The function should find the first pair
 // where the sum is 0. Return an array that includes both
 // values that sum to zero or undefined if a pair does not exit
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) return [arr[i], arr[j]];
+    }
+  }
+}
