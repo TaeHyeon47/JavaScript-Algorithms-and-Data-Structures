fd264d9bdf8c8b2ed40d4372a0478127bd1e153d
Tae Hyeon Jo <hillskuti@naver.com>
2023-08-21 20:30:30 +0900

    sumZero 코드 리팩토링

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -81,3 +81,18 @@ function sumZero(arr) {
     }
   }
 }
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
