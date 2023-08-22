86467fc1fccc8a36a68768e4c74ccde979e27a26
Tae Hyeon Jo <hillskuti@naver.com>
2023-08-22 22:07:11 +0900

    애너그램 도전과제 한글 해석

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -66,6 +66,9 @@ same([1, 2, 3, 2], [9, 1, 4, 4]);
 // second string is an anagram of the first. An anagram is
 // a word, phrase, or name formed by rearranging the
 // letters of another, such as cinema, formed from iceman.
+// 두 개의 문자열이 주어졌을 때, 두 번째 문자열이 첫 번째 문자열의 애너그램인지
+// 확인하는 함수를 작성합니다. 애너그램은 다른 글자의 글자를 재배열하여 형성된 단어,
+// 구 또는 이름입니다. (예시: cinema -> iceman)
 
 function validAnagram(first, second) {
   if (first.length !== second.length) {
