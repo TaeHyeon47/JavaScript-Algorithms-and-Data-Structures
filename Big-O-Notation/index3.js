34ebac993e7dc52858e1e38860e8d24b7e9e5923
Tae Hyeon Jo <hillskuti@naver.com>
2023-08-31 18:39:57 +0900

    슬라이딩 윈도우 문제 정의

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -210,3 +210,10 @@ function countUniqueValues(arr) {
     }
   }
 }
+
+// 슬라이딩 윈도우 (Sliding window) 문제 정의
+// This pattern involves creating a window which can either be an array or
+// window which can either be an array or number from one position to another
+// Depending on a certain condition, the window either increases or closes
+// (and a new window is created)
+// Very useful for keeping track of a subset of data in an array/string etc.
