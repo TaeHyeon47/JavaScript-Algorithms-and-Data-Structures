509f493068b38ecb41ef3aed325f588f9f088951
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-03 23:30:51 +0900

    슬라이딩 윈도우 예시 정의

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -217,3 +217,15 @@ function countUniqueValues(arr) {
 // Depending on a certain condition, the window either increases or closes
 // (and a new window is created)
 // Very useful for keeping track of a subset of data in an array/string etc.
+
+// 예시
+// Write a function called maxSubarraySum which accepts
+// an array of integers and a number called 'n.' The function
+// should calculate the maximum sum of 'n' consecutive
+// elements in the array.
+
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2) // 10
+// maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4) // 17
+// maxSubarraySum([4, 2, 1, 6], 1) // 6
+// maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
+// maxSubarraySum([], 4) // null
