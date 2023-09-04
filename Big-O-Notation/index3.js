3681daffa28804336cc866e4dd19034ea3651645
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-04 20:53:21 +0900

    슬라이딩 윈도우(기준점 간 이동 배열 패턴) 솔루션 1

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -229,3 +229,20 @@ function countUniqueValues(arr) {
 // maxSubarraySum([4, 2, 1, 6], 1) // 6
 // maxSubarraySum([4, 2, 1, 6, 2], 4) // 13
 // maxSubarraySum([], 4) // null
+
+function maxSubarraySum(arr, num) {
+  if (num > arr.length) {
+    return null;
+  }
+  var max = -Infinity;
+  for (let i = 0; i < arr.length - num + 1; i++) {
+    temp = 0;
+    for (let j = 0; j < num; j++) {
+      temp += arr[i + j];
+    }
+    if (temp > max) {
+      max = temp;
+    }
+  }
+  return max;
+}
