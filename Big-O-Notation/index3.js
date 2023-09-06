63102663b538d81945b94a662c2952649de961e9
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-06 23:34:12 +0900

    슬라이딩 윈도우 코드 리팩토링

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -233,19 +233,41 @@ function countUniqueValues(arr) {
 // maxSubarraySum([], 4) // null
 
 // 솔루션 1
+// Time Complexity - O(N^2)
 function maxSubarraySum(arr, num) {
   if (num > arr.length) {
     return null;
   }
   var max = -Infinity;
+  console.log('arr.length', arr.length);
   for (let i = 0; i < arr.length - num + 1; i++) {
     temp = 0;
     for (let j = 0; j < num; j++) {
+      console.log('num', num);
+
       temp += arr[i + j];
+      console.log('temp', temp);
     }
     if (temp > max) {
       max = temp;
     }
+    console.log('max', max);
   }
   return max;
+}
+
+// 리팩토링 코드
+function maxSubarraySum(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (arr.length < num) return null;
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
 }
