bc18a1e496bf91521442ff2344eb98ef83d499a4
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-11 20:27:30 +0900

    분할과 정복 패턴 예시

--- Big-O-Notation/index3.js
+++ Big-O-Notation/index3.js
@@ -278,3 +278,11 @@ function maxSubarraySum(arr, num) {
 // process with a subset of data.
 
 // This pattern can tremendously decrease time complexity
+
+// An Example
+// Given a sorted array of integers, write a function called search,
+// that accepts a value and returns the index where the value passed
+// to the function is located. If the value is not found, return -1
+// search([1, 2, 3, 4, 5, 6], 4) // 3
+// search([1, 2, 3, 4, 5, 6], 6) // 5
+// search([1, 2, 3, 4, 5, 5], 11) // -1
