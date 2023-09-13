aed45225a63be361fc36fd16b2e792d9b933c35e
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-13 22:35:01 +0900

    재귀 함수 우화, 정의, 사용이유 정리

---
+++ Big-O-Notation/recursion.js
@@ -0,0 +1,17 @@
+// 재귀를 이해하기 쉬운 우화!
+// 마틴과 화난 드래곤 이야기... (알고리즘 41강 참고)
+// No Odds (3142 5798 6550 5914)
+// No Odds (5798 6550 5914)
+// No Odds (6550 5914)
+// No Odds (5914)
+// No Odds ()
+
+// What is recursion?
+//* A process (a function in our case) that calls itself (자기자신을 호출하는 함수)
+
+// 재귀는 많은 곳에서 활용된다
+// JSON.parse / JSON.stringify
+// document.getElementById and DOM traversal algorithms
+// Object traversal(객체 순회)
+// We will see it with more complex data structures (복잡한 데이터 구조)
+// It's sometimes a cleaner alternative to iteration (때로는 반복 대신 재귀를 사용하는게 더 깔끔)
