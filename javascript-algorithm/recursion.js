be50cb6956d855ea4949ec86d536b20376f75906
Tae Hyeon Jo <hillskuti@naver.com>
2023-09-21 07:57:31 +0900

    첫번째 재귀함수 설명 추가

--- javascript-algorithm/recursion.js
+++ javascript-algorithm/recursion.js
@@ -25,6 +25,7 @@
 // 스택이라는 개념을 책상에 쌓여있는 종이 더미에 비유하는 이유는 무언가를 꼭대기에 배치하기 때문이다.
 // 우리는 무언가를 꺼낼 때도, 꼭대기에서부터 꺼낸다.
 
+// 아래 코드는 크롬 브라우저에서 Call Stack이 동작하는 원리는 설명하는 예시 코드로 작성함.
 function takeShower() {
   return 'Showering!';
 }
@@ -47,7 +48,18 @@ function wakeUp() {
 
 wakeUp();
 
-/// 첫번째 재귀 함수
+//? 첫번째 재귀 함수
+// How recursive functions work
+// Invoke the same function with a different input until you reach your base case!
+
+// Two essential parts of a recursive function!
+// 아래의 두가지 요소는 재귀 함수에 필수 요소이다.
+// - Base Case
+// - Different Input
+
+// Base Case란?
+// The condition when the recursion ends. (재귀를 종료하는 문장)
+//* This is the most important concept to understand
 
 function countDown(num) {
   if (num <= 0) {
@@ -58,3 +70,22 @@ function countDown(num) {
   num--;
   countDown(num);
 }
+
+// print 3
+// countDown(2);
+// print 2
+// countDown(1);
+// print 1
+// countDown(0); - 종료점
+// print "All Done"
+
+// 아래와 같이 재귀를 사용하지 않더라도 비슷한 작업을 할 수 있다.
+// 하지만, 상기의 정리된 호출과 같이 그 과정은 다른 것을 확인할 수 있다.
+function countDown(num) {
+  for (var i = num; i > 0; i--) {
+    console.log(i);
+  }
+  console.log('All done!');
+}
+
+countDown(5);
