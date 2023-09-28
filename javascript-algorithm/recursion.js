9400243190fb62f5ad300cc54b61512cbac565ab
TaeHyeon47 <hillskuti@naver.com>
2023-09-28 20:34:32 +0900

    순수 재귀를 통한 문제 해결

--- javascript-algorithm/recursion.js
+++ javascript-algorithm/recursion.js
@@ -160,3 +160,28 @@ function collectOddValues(arr){
 }
 
 collectOddValues([1,2,3,4,5,6,7,8,9])
+
+/////////////////////////////////////////////////////
+//? 순수 재귀
+
+function collectOddValues(arr){
+  let newArr = [];
+  
+  if(arr.length === 0) {
+      return newArr;
+  }
+      
+  if(arr[0] % 2 !== 0){
+      newArr.push(arr[0]);
+  }
+      
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
+
+collectOddValues([1,2,3,4,5])
+// [1].concat(collectOddValues([2,3,4,5]))
+//              [].concat(collectOddValues([3,4,5]))
+//                          [3].concat(collectOddValues([4,5]))
+//                                        [].concat(collectOddValues([5]))
+//                                                     [5].concat(collectOddValues([]))
