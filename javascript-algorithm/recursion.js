751737ec64032347be9b3a4a60f41d06d2600a72
TaeHyeon47 <hillskuti@naver.com>
2023-09-28 22:04:33 +0900

    알고리즘 코딩 테스트 power 문제 (나의 솔루션)

--- javascript-algorithm/recursion.js
+++ javascript-algorithm/recursion.js
@@ -193,3 +193,18 @@ collectOddValues([1,2,3,4,5])
 // slice, substr, or substring to make copies of strins
 // 3. To make copies of objects use Object.assign, or the spread operator
 
+///////////////////////////////////////////////
+//? 알고리즘 문제 
+
+// power
+// 밑과 지수를 받아들이는 power라는 함수를 작성합니다. 이 함수는 밑의 거듭제곱을 지수로 반환해야 합니다. 
+// 이 함수는 Math.pow()의 기능을 모방해야 합니다. 음의 밑과 지수에 대해서는 신경쓰지 마세요.
+
+// power(2,0) // 1
+// power(2,2) // 4
+// power(2,4) // 16
+
+function power(base, exponent){
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
