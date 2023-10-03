2094519bf0a6bbf960b83f9f1b2f6d362e8be843
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-03 23:18:28 +0900

    정렬을 배우는 이유

--- javascript-algorithm/sortingAlgorithms.js
+++ javascript-algorithm/sortingAlgorithms.js
@@ -8,3 +8,8 @@
 // 정렬이란?
 // 컬렉션(collection)의 항목을 재배열하는 과정을 의미한다.
 // 예를 들어 문자열을 하나 고른 후에 문자열 내의 각 문자를 정렬하는 걸 예로 들 수 있다.
+
+// Why do we need to learn this?
+// 1. Sorting is an incredibly common task, so it's good to know how it works
+// 2. There are many different ways to sort things, and different techniques
+// have their own advantages and disadvantages.
