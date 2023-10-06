57206b904d9621ff647c66e915eab31b5945d768
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-06 19:38:46 +0900

    문자열 길이를 통한 정렬 예시 추가

--- javascript-algorithm/sortingAlgorithms.js
+++ javascript-algorithm/sortingAlgorithms.js
@@ -51,3 +51,10 @@ function numberCompare(num1, num2) {
 }
 
 [6, 4, 15, 10].sort(numberCompare); // [4, 6, 10, 15]
+
+// 문자열 길이를 기준으로 정렬하는 것도 가능
+function compareByLen(str1, str2) {
+  return str1.length - str2.length;
+}
+
+['Steele', 'Colt', 'Data Structures', 'Algorithms'].sort(compareByLen);
