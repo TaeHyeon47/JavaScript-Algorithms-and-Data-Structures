8be33ecf617070d340b26a1aab06cbc4a15c19a8
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-11 22:25:04 +0900

    버블정렬 최적화 코드

--- javascript-algorithm/sortingAlgorithms.js
+++ javascript-algorithm/sortingAlgorithms.js
@@ -88,16 +88,11 @@ const swap = (arr, idx1, idx2) => {
   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
 };
 
-// BubbleSort Pseudocode
-// 1. Start looping from with a variable called i the end of the array towards the beginning.
-// 2. Start an inner loop with a variable called j from the beginning until i - 1
-// 3. If arr[j] is greater than arr[j+1], swap those two values!
-// 4. Return the sorted array
-
-// 미최적화 버블정렬
+// 미최적화 버블 정렬
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         //SWAP!
         let temp = arr[j];
@@ -110,3 +105,35 @@ function bubbleSort(arr) {
 }
 
 bubbleSort([37, 45, 29, 8]);
+// [8, 29, 37, 45] 37 45
+// [37, 45, 29, 8] 45 29
+// [37, 29, 45, 8] 45 8
+// [37, 29, 8, 45] 45 undefined
+// [37, 29, 8, 45] 37 29
+// [29, 37, 8, 45] 37 8
+// [29, 8, 37, 45] 37 45
+// [29, 8, 37, 45] 45 undefined
+// ...
+//* 미최적화 버블정렬 코드는 undefined가 생겨난다. 따라서 아래와 같은 최적화가 필요하다.
+
+// BubbleSort Pseudocode
+// 1. Start looping from with a variable called i the end of the array towards the beginning.
+// 2. Start an inner loop with a variable called j from the beginning until i - 1
+// 3. If arr[j] is greater than arr[j+1], swap those two values!
+// 4. Return the sorted array
+
+// 최적화 버블 정렬
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        //SWAP!
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
