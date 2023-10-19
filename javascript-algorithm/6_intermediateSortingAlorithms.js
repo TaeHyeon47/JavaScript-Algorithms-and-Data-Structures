adb364c0362b27faab4452a670ac16816eb1d8bb
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-19 23:02:24 +0900

    중급 알고리즘 학습목표 및 이유 정리

---
+++ javascript-algorithm/6_intermediateSortingAlorithms.js
@@ -0,0 +1,15 @@
+/////////////////////
+//? Objectives
+// ● Understand the limitations of the sorting algorithms we've learned so far
+// ● Implement merge sort
+// ● Implement quick sort
+// ● Implement radix sort
+
+//? Why learn this?
+// ● The sorting algorithms we've learned so far don't scale well (20개 이하 항목 배열과 같이 작은 규모에서 잘 작동)
+// ● Try out bubble sort on an array of 100000 elements, it will take quite some time!
+
+//? Faster sorts
+// ● There is a family of sorting algorithms that can improve time complexity from O(N²) to O(n log n)
+// ● There's a tradeoff between efficiency and simplicity
+// ● The more efficient algorithms are much less simple, and generally take longer to understand
