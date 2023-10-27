9a32d6d4320a8465e83b2400582d32d2ef232cc3
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-27 16:21:27 +0900

    static 클래스 메소드 및 클래스 내용 정리

--- javascript-algorithm/7_data-structure.js
+++ javascript-algorithm/7_data-structure.js
@@ -116,3 +116,80 @@ let firstStudent2 = new Student('Colt', 'Steele', 1);
 let secondStudent2 = new Student('Blue', 'Steele', 2);
 
 secondStudent2.addScore(92);
+
+// Class Methods
+
+// ● 'static' 키워드는 클래스에 종속되는 반면 클래스의 개별 인스턴스 메소드에는 반드시 종속적일 필요가 없는 메소드 혹은 기능들을 생성.
+// ● MDN 정의 : 'static' 키워드는 클래스의 Static 메소드를 정의합니다. Static 메소드는 클래스의 인스턴스화 없이도 호출될 수 있으며 클래스 인스턴스를 통해서 호출될 수 없습니다.
+// 이들은 어플리케이션을 위한 유틸리티 기능을 생성하기 위해 자주 사용됩니다.
+
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return 'YOU ARE EXPELLED!!!!';
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+  calculateAverage() {
+    let sum = this.scores.reduce(function (a, b) {
+      return a + b;
+    });
+    return sum / this.scores.length;
+  }
+  static EnrollStudents() {
+    return 'ENROLLING STUDENTS!';
+  }
+}
+
+let firstStudent5 = new Student('Colt', 'Steele', 1);
+let secondStudent5 = new Student('Blue', 'Steele', 2);
+Student.EnrollStudents();
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+
+// 아래와 같이 인자를 넣기 위해 클래스 메소드(static)이 필요
+// Point.distance(p1, p2) - 7.0710678118654755
+
+// ● We will be using the constructor and instance methods quite a bit!
+// ● We will almost never be using static methods.
+
+//* One gotcha with 'this'
+// ● Inside all of our instance methods and constructor, the keyword 'this' refers to the object created from that class (also known as an instance).
+
+// Recap
+// ● Classes are blueprints that when created make objects known as instances.
+// ● Classes are created with the new keyword.
+// ● The constructor function is a special function that gets run when the class is instantiated.
+// ● Instance methods can be added to classes similar to methods in objects.
+// ● Class methods can be added using the static keyword.
+// 'new'를 통해 클래스를 인스턴스화 시키게 되면, constructor가 먼저 동작하게 된다.
