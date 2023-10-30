76ccbf3af0f17aae14815ab7bcb7d7be175cef48
Tae Hyeon Jo <hillskuti@naver.com>
2023-10-30 22:38:56 +0900

    단일 연결 리스트 스타터 코드

--- javascript-algorithm/8_Singly-Linked-List.js
+++ javascript-algorithm/8_Singly-Linked-List.js
@@ -30,3 +30,51 @@
 // ● Can quickly be accessed at a specific index
 
 // Array의 경우 새로운 데이터를 중간에 추가하면 인덱스가 밀리는 물결 효과를 감수해야한다.
+
+//? 코드 스타터와 push 메소드 소개
+// piece of data - val
+// reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+// var first = new Node('Hi');
+// first.next = new Node('there');
+// first.next.next = new Node('how');
+// first.next.next.next = new Node('are');
+// first.next.next.next.next = new Node('you');
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList();
+list.push('HELLO');
+
+// Pushing pseudocode
+// ● This function should accept a value
+// ● Create a new node using the value passed to the function
+// ● If there is no head property on the list, set the head and tail to be the newly created node
+// ● Otherwise set the next property on the tail to be the new node and set the tail property on the list to be the newly created node
+// ● Increment the length by one
+// ● Return the linked list
