67b074744b03399aad29615c780b823c13178306
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-01 21:09:47 +0900

    단일 연결 리스트 pop 메소드 정의

--- javascript-algorithm/8_Singly-Linked-List.js
+++ javascript-algorithm/8_Singly-Linked-List.js
@@ -73,13 +73,6 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
-  traverse() {
-    var current = this.head;
-    while (current) {
-      console.log(current.val);
-      current = current.next;
-    }
-  }
 }
 
 var list = new SinglyLinkedList();
@@ -125,3 +118,40 @@ class SinglyLinkedList {
 // ● Set the tail to be the 2nd to last node
 // ● Decrement the length of the list by 1
 // ● Return the value of the node removed
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
