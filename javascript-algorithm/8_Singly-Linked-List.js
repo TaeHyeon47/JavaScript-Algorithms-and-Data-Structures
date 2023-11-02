5ec6c8747581d3950ffbc0def5b67617def0cefd
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-02 21:02:26 +0900

    unshift 의사 및 구현코드

--- javascript-algorithm/8_Singly-Linked-List.js
+++ javascript-algorithm/8_Singly-Linked-List.js
@@ -213,3 +213,75 @@ class SinglyLinkedList {
     return currentHead;
   }
 }
+
+//? Unshifting
+// ● Adding a new node to the beginning of the Linked List!
+
+// Unshifting pseudocode
+// ● This function should accept a value
+// ● Create a new node using the value passed to the function
+// ● If there is no head property on the list, set the head and tail to be the newly created node
+// ● Otherwise set the newly created node's next property to be the current head property on the list
+// ● Set the head property on the list to be that newly created node
+// ● Increment the length of the list by 1
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    var current = this.head;
+    var newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    var currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      // else를 넣지 않으면 head, tail가 null인 상황에서 무한루프 코드를 만들어 버린다.
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
