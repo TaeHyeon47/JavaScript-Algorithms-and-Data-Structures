251af10d0640151ee9a4381148a4ef3e3ac8ce4f
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-09 23:19:13 +0900

    Stack 정의

---
+++ javascript-algorithm/9_Stack-Queue.js
@@ -0,0 +1,17 @@
+/////////////////////////////////////////
+//? 스택 (Stacks) + 큐 (Queues)
+
+// Objectives
+// ● Define what a stack is
+// ● Understand use cases for a stack
+// ● Implement operations on a stack data structure
+
+//? What is a stacks?
+// ● A LIFO data structure!
+// ● The last element added to the stack will be the first element removed from the stack
+
+// How is it used?
+// ● Think about a stack of plates, or a stack of markers, or a stack of... anything.
+// ● As you pile it up the last thing (or the topmost thing) is what gets removed first.
+
+// 강의 151 사진 참조
