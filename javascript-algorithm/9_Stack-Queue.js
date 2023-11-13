0b38a394bc79b3c87a13a883dac9b8c1cf253d33
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-13 21:29:47 +0900

    자바스크립트 내장 배열로 스택 만들기

--- javascript-algorithm/9_Stack-Queue.js
+++ javascript-algorithm/9_Stack-Queue.js
@@ -21,7 +21,33 @@
 // ● Undo / Redo
 // ● Routing (the history object) is treated like a stack!
 
+//? 자바스크립트 내장 배열로 스택 만들기
 // There is more than one way of implementing a stack
+
+// 1.
+var stack = [];
+stack.push('google');
+stack.push('instagram');
+stack.push('youtube');
+// stack ['google', 'instagram', 'youtube']
+stack.pop(); // youtube
+stack.pop(); // instagram
+stack.push('amazon');
+stack.pop(); // amazon
+stack.pop(); // google
+
+// 2.
+var stack = [];
+stack.unshift('create new file');
+stack.unshift('resized file');
+stack.unshift('cloned out wrinkle');
+// stack ['cloned out wrinkle', 'resized file', 'create new file']
+stack.shift(); // cloned out wrinkle
+stack.shift(); // resized file
+stack.shift(); // create new file
+
+// 배열의 재배치 관점에서 본다면 unshift보다 push를 사용하는 것이 낫다.
+// 코드의 효율성을 보았을 때 배열을 스택으로 이용하지 않는 경우가 더 많다.
 
 class Node {
   constructor(value) {
