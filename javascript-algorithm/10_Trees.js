d0e1c5a6f81aaeda6af91a005f3eca0dc214d7f2
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-20 23:08:38 +0900

    Tree 정의

---
+++ javascript-algorithm/10_Trees.js
@@ -0,0 +1,21 @@
+//? Objectives
+// ● Define what a tree is
+// ● Compare and contrast trees and lists
+// ● Explain the differences between trees, binary trees, and binary search trees
+// ● Implement operations on binary search trees
+
+//? What is a tree?
+//* ● A data structure that consists of nodes in a parent / child relationship
+
+// A Singly Linked List : sort of a special case of a tree
+// ● 노드가 자신보다 더 낮은 곳에 있지 않은 다른 노드를 가리키면 Tree가 아님.
+// ● 노드는 부모-자식 관계에 따라서 자식인 노드만을 가리킬 수 있음.
+// ● 트리는 출발 지점이 하나여야 한다.
+
+//? Tree Terminology
+//* ● Root - The top node in a tree.
+//* ● Child - A node directly connected to another node when moving away from the Root.
+//* ● Parent - The converse notion of a child.
+//* ● Siblings - A group of nodes with the same parent.
+//* ● Leaf - A node with no children.
+//* ● Edge - The connection between one node and another.
