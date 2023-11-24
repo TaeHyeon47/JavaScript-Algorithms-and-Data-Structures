f84fe2efcfd4cf85444e307d14a5de163c83e41c
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-24 22:07:23 +0900

    트리 클래스

--- javascript-algorithm/10_Trees.js
+++ javascript-algorithm/10_Trees.js
@@ -28,3 +28,24 @@
 // ● Artificial Intelligence
 // ● Folders in Operating Systems
 // ● Computer File Systems
+
+//? 트리 클래스
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
+
+var tree = new BinarySearchTree();
+tree.root = new Node(10);
+tree.root.right = new Node(15);
+tree.root.left = new Node(7);
+tree.root.left.right = new Node(9);
