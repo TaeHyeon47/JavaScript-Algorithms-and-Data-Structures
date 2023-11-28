70e25e6938f0112442e899d662d5bd483f044e92
Tae Hyeon Jo <hillskuti@naver.com>
2023-11-28 20:43:04 +0900

    이진 탐색 트리 정의 및 동작 원리

--- javascript-algorithm/10_Trees.js
+++ javascript-algorithm/10_Trees.js
@@ -50,7 +50,17 @@ tree.root.right = new Node(15);
 tree.root.left = new Node(7);
 tree.root.left.right = new Node(9);
 
-//? 트리(Tree) Insert 메소드
+//? 이진(Binary) 트리(Tree)의 개념
+// ● 트리에도 다양한 종류가 있다
+// ● 한 노드당 최대 2개의 자식 노드를 가질 수 있다. 즉, 0, 1, 2개의 자식을 가진다.
+// ● 이진(Binary) 검색(Search) 트리(Tree)는 데이터를 비교해서 정렬 가능하게 한다.
+
+// How BSTs work
+// ● Every parent node has at most two children
+// ● Every node to the left of a parent node is always less than the parent
+// ● Every node to the right of a parent node is always greater than the parent
+
+//? 이진(Binary) 트리(Tree) Insert 메소드
 
 class Node {
   constructor(value) {
