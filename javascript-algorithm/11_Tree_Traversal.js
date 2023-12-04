19b6380fcf56140f35cbe69157e0ad9796ca1ed9
Tae Hyeon Jo <hillskuti@naver.com>
2023-12-04 23:30:18 +0900

    트리 순회 contains

--- javascript-algorithm/11_Tree_Traversal.js
+++ javascript-algorithm/11_Tree_Traversal.js
@@ -50,4 +50,19 @@ class BinarySearchTree {
     if (!found) return undefined;
     return current;
   }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
